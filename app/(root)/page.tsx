bef625ad33dfc8261ed15df9e17545ee91d46bfa
Hussen <ha0959943@gmail.com>
2025-07-12 14:28:57 +0300

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-karla">welcome to loom clone</h1>
+    </div>
+  );
+};
+
+export default Page;
